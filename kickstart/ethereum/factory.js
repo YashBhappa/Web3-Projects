9b3dbf1cc27ad13897d13b2823c52a858bd4ce3f
Yash Bhappa <ybhappa@gmail.com>
2024-12-13 09:23:48 +0530

    grid layouts

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x90e2b94532734E2999D4919Ea1b5263Bf35d4B0d'
+  '0xB07Ff2bB7a4905FCCB65305F8F877fE5F0b44d8b'
 );
 
 export default instance;
