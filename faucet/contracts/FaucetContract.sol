f3bc01806748393f96ebe24e7af250178f4af4a0
Yash Bhappa <ybhappa@gmail.com>
2024-12-14 09:23:32 +0530

    getFunders view accessors

--- faucet/contracts/FaucetContract.sol
+++ faucet/contracts/FaucetContract.sol
@@ -10,4 +10,13 @@ contract Faucet {
   function addFunds() external payable {
     funders.push(msg.sender);
   }
+
+  function getAllFunders() public view returns (address[] memory) {
+    return funders;
+  }
+
+  function getFunderAtIndex(uint8 index) external view returns(address) {
+    address[] memory _funders = getAllFunders();
+    return _funders[index];
+  }
 }
