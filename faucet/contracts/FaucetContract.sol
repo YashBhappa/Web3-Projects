d17ec008fc9108c6ebc323f5b3ec478c6e49c8a8
Yash Bhappa <ybhappa@gmail.com>
2024-12-14 09:20:51 +0530

    add funds and funders

--- faucet/contracts/FaucetContract.sol
+++ faucet/contracts/FaucetContract.sol
@@ -3,17 +3,11 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 
-  // this is a special function
-  // it's called when you make a tx that doesn't specify
-  // function name to call
-
-  // External function are part of the contract interface
-  // which means they can be called via contracts and other txs
+  address[] public funders;
 
   receive() external payable {}
-}
 
-// Block info
-// Nonce - a hash that when combined with the minHash proofs that
-// the block has gone through proof of work(POW)
-// 8 bytes => 64 bits
+  function addFunds() external payable {
+    funders.push(msg.sender);
+  }
+}
