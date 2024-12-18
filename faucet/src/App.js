5133c56798141ff89fedfab51fedca23f5b5cef5
Yash Bhappa <ybhappa@gmail.com>
2024-12-18 12:15:38 +0530

    load contract

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Web3 from "web3";
 import detectEthereumProvider from '@metamask/detect-provider'
+import { loadContract } from "./utils/load-contract";
 
 
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
-    web3: null
+    web3: null,
+    contract: null
   })
 
   const [account, setAccount] = useState(null)
@@ -16,12 +18,13 @@ function App() {
   useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider()
+      const contract = await loadContract("Faucet")
 
       if (provider) {
-        provider.request({method: "eth_requestAccounts"})
         setWeb3Api({
           web3: new Web3(provider),
-          provider
+          provider,
+          contract
         })
       } else {
         console.error("Please, install Metamask.")
@@ -44,17 +47,29 @@ function App() {
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
-          <span>
-            <strong>Account: </strong>
-          </span>
-          <h1>
-            { account ? account : "not connected" }
-          </h1>
-          <div className="balance-view is-size-2">
+          <div className="is-flex is-align-items-center">
+            <span>
+              <strong className="mr-2">Account: </strong>
+            </span>
+              { account ?
+                <div>{account}</div> :
+                <button
+                  className="button is-small"
+                  onClick={() =>
+                    web3Api.provider.request({method: "eth_requestAccounts"}
+                  )}
+                >
+                  Connect Wallet
+                </button>
+              }
+          </div>
+          <div className="balance-view is-size-2 my-4">
             Current Balance: <strong>10</strong> ETH
           </div>
-          <button className="btn mr-2">Donate</button>
-          <button className="btn">Withdraw</button>
+          <button
+            className="button is-link mr-2">Donate</button>
+          <button
+            className="button is-primary">Withdraw</button>
         </div>
       </div>
     </>
