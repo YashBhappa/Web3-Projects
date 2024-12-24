ebb11e9085c28b6b03d067a574b709619aa54125
Yash Bhappa <ybhappa@gmail.com>
2024-12-24 19:11:12 +0530

    targetting network

--- marketplace/pages/marketplace/index.js
+++ marketplace/pages/marketplace/index.js
@@ -1,6 +1,6 @@
 
 
-import { CourseList } from "@components/ui/course"
+import { CourseCard, CourseList } from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
 import { WalletBar } from "@components/ui/web3"
@@ -18,10 +18,20 @@ export default function Marketplace({courses}) {
           address={account.data}
           network={network.data}
         />
+        "Current" {`${network.data}`}
+        "Target" {`${network.target}`}
+        "Is Supported" {`${network.isSupported}`}
       </div>
       <CourseList
         courses={courses}
-      />
+      >
+      {course =>
+        <CourseCard
+          key={course.id}
+          course={course}
+        />
+      }
+      </CourseList>
     </>
   )
 }
