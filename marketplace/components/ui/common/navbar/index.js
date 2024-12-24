b632b634f79d715f9ed230d3d44f5ba704a8a69b
Yash Bhappa <ybhappa@gmail.com>
2024-12-24 19:10:29 +0530

    chain ID

--- marketplace/components/ui/common/navbar/index.js
+++ marketplace/components/ui/common/navbar/index.js
@@ -1,15 +1,15 @@
 
 
-
-
 import { useWeb3 } from "@components/providers"
 import Link from "next/link"
 import { Button } from "@components/ui/common"
-import { useAccount } from "@components/web3/hooks/useAccount"
+import { useAccount } from "@components/hooks/web3/useAccount"
+import { useRouter } from "next/router"
 
 export default function Footer() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3()
   const { account } = useAccount()
+  const { pathname } = useRouter()
 
   return (
     <section>
@@ -23,7 +23,7 @@ export default function Footer() {
                   Home
                 </a>
               </Link>
-              <Link href="/" >
+              <Link href="/marketplace" >
                 <a
                   className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Marketplace
@@ -70,6 +70,7 @@ export default function Footer() {
         </nav>
       </div>
       { account.data &&
+        !pathname.includes("/marketplace") &&
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="text-white bg-indigo-600 rounded-md p-2">
             {account.data}
