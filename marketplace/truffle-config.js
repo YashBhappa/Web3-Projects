4f1d07a4f8c5c3984fa9739b0c305876a48a20fe
Yash Bhappa <ybhappa@gmail.com>
2024-12-28 01:13:02 +0530

    truffle ropsten -> sepholia

--- marketplace/truffle-config.js
+++ marketplace/truffle-config.js
@@ -1,17 +1,14 @@
-
-
-module.exports = {
-  contracts_build_directory: "./public/contracts",
-  networks: {
-    development: {
-     host: "127.0.0.1",
-     port: 7545,
-     network_id: "*",
-    }
-  },
-  compilers: {
-    solc: {
-      version: "0.8.4",
-    }
-  }
+sepolia: {
+  provider: () =>
+    new HDWalletProvider({
+      mnemonic: {
+        phrase: ""
+      },
+      providerOrUrl: "wss://sepolia.infura.io/ws/v3/YOUR-PROJECT-ID",
+      addressIndex: 0
+    }),
+  network_id: "11155111",
+  gasPrice: 2500000000,
+  networkCheckoutTimeout: 10000,
+  timeoutBlocks: 200
 }
