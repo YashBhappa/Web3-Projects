81ae840d3dcfcd6a3e3ca8711483d255fba1015e
Yash Bhappa <ybhappa@gmail.com>
2024-12-27 16:12:07 +0530

    improvements

--- marketplace/pages/marketplace/courses/owned.js
+++ marketplace/pages/marketplace/courses/owned.js
@@ -6,25 +6,32 @@ import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 import { getAllCourses } from "@content/courses/fetcher";
+import { useRouter } from "next/router";
 
 export default function OwnedCourses({courses}) {
+  const router = useRouter()
   const { account } = useAccount()
   const { ownedCourses } = useOwnedCourses(courses, account.data)
 
   return (
     <>
-      <div className="py-4">
-        <MarketHeader />
-      </div>
+      <MarketHeader />
       <section className="grid grid-cols-1">
-        <OwnedCourseCard>
-          <Message>
-            My custom message!
-          </Message>
-          <Button>
-            Watch the course
-          </Button>
-        </OwnedCourseCard>
+        { ownedCourses.data?.map(course =>
+          <OwnedCourseCard
+            key={course.id}
+            course={course}
+          >
+            {/* <Message>
+              My custom message!
+            </Message> */}
+            <Button
+              onClick={() => router.push(`/courses/${course.slug}`)}
+            >
+              Watch the course
+            </Button>
+          </OwnedCourseCard>
+        )}
       </section>
     </>
   )
