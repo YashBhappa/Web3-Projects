2bb71874fbf5f264660eb1d11396d3d6d67e78a7
Yash Bhappa <ybhappa@gmail.com>
2024-12-28 01:15:41 +0530

    infura setup + mnemonic + deploy

--- marketplace/truffle-config.js
+++ marketplace/truffle-config.js
@@ -1,14 +1,43 @@
-sepolia: {
-  provider: () =>
-    new HDWalletProvider({
-      mnemonic: {
-        phrase: ""
-      },
-      providerOrUrl: "wss://sepolia.infura.io/ws/v3/YOUR-PROJECT-ID",
-      addressIndex: 0
-    }),
-  network_id: "11155111",
-  gasPrice: 2500000000,
-  networkCheckoutTimeout: 10000,
-  timeoutBlocks: 200
+
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const keys =  require("./keys.json")
+
+module.exports = {
+  contracts_build_directory: "./public/contracts",
+  networks: {
+    development: {
+     host: "127.0.0.1",
+     port: 7545,
+     network_id: "*",
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: keys.MNEMONIC
+          },
+          providerOrUrl: `https://ropsten.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+          addressIndex: 0
+        }),
+      network_id: 3,
+      gas: 5500000, // Gas Limit, How much gas we are willing to spent
+      gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200 // number of blocks before deployment times out
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.4",
+    }
+  }
 }
+
+// 5500000 * 20000000000 = 110000000000000000 = 0,11 ETH => 334 USD
+
+
+// transaction hash:    0xd6fde57764ae89d6799d355abd025d861e500ea797ebaf08d2d09fbad5561a9b
+// contract address:    0x55a4202C5854AE44282d6824feEDa40B46E17f89
+
+
+// 0,00000002 * 1494678
