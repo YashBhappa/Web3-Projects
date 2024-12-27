d1497cd103f497f3d9375438e40e61bc2dcb5e0c
Yash Bhappa <ybhappa@gmail.com>
2024-12-27 16:11:39 +0530

    course owner state

--- marketplace/pages/marketplace/courses/owned.js
+++ marketplace/pages/marketplace/courses/owned.js
@@ -1,11 +1,15 @@
 
 
+import { useAccount, useOwnedCourses } from "@components/hooks/web3";
 import { Button, Message } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
+import { getAllCourses } from "@content/courses/fetcher";
 
-export default function OwnedCourses() {
+export default function OwnedCourses({courses}) {
+  const { account } = useAccount()
+  const { ownedCourses } = useOwnedCourses(courses, account.data)
 
   return (
     <>
@@ -24,6 +28,15 @@ export default function OwnedCourses() {
       </section>
     </>
   )
+}
+
+export function getStaticProps() {
+  const { data } = getAllCourses()
+  return {
+    props: {
+      courses: data
+    }
+  }
 }
 
 OwnedCourses.Layout = BaseLayout
